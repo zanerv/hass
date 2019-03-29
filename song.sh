afb9edc528c4d187826d6ffe74bd2aeb8af240bf
zanerv <zanerv@gmail.com>
2019-03-29 17:24:11 +0100

    fuuu hassio

--- song.sh
+++ song.sh
@@ -4,5 +4,5 @@ playlist=7708 #Get playlist id: curl -s  http://plex:32400/playlists/all
 song=$(curl -s http://${IP}:32400/playlists/$playlist/items \
  |grep "parts"|awk -F '"' '{print "http://'${IP}':32400" $4}'|shuf |head -1)
 
-curl -s -X POST -H "Content-Type: application/json" -d '{"attributes": {"friendly_name":  "Song"}, "entity_id": "input_boolean.song", "state": "'"$song"'"}'\
+curl -s -X POST -H "Authorization: Bearer $HASS_TOKEN" -H "Content-Type: application/json" -d '{"attributes": {"friendly_name":  "Song"}, "entity_id": "input_boolean.song", "state": "'"$song"'"}'\
  http://localhost:8123/api/states/input_boolean.song >/dev/null 2>&1
