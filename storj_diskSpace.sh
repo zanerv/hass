da2fa503d7040c2b811c975db7a61f0e5300e040
zanerv <zanerv@gmail.com>
2019-10-13 00:04:59 +0200

    introducing more bugs

--- storj_diskSpace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#!/bin/bash
-id=$(curl -s -H "Content-Type: application/json" --unix-socket /var/run/docker.sock -X POST http:/v1.24/containers/storagenode/exec -d '{ "AttachStdin":false,"AttachStdout":true,"AttachStderr":true, "Tty":false, "Cmd":["/usr/bin/curl", "-s", "127.0.0.1:14002/api/dashboard"]}' | jq -r .Id)
-curl -s -H "Content-Type: application/json" --unix-socket /var/run/docker.sock -X POST http:/v1.24/exec/${id}/start -d '{ "Detach":false,"Tty":true}' --output - | jq .data.diskSpace.used
-
